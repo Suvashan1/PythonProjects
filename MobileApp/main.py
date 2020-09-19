384d04c2faf4b9660fdc987694c7afcbf6c5a5e4
Suvashan1 <suvapillay@gmail.com>
2020-09-19 20:55:22 +0200

    path change

--- MobileApp/main.py
+++ MobileApp/main.py
@@ -2,11 +2,11 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file('design.kv')
+Builder.load_file('C:\\Users\\Suvashan\\Desktop\\stm32\\PythonProjects\\Mobile Mood App\\design.kv')
 
 class LoginScreen(Screen):          #Third highest priority
-    def signup(self):
-        print("Hey")  
+    def sign_up(self):
+        print("Hey friend")  
 
 class RootWidget(ScreenManager):   #Second highest priority
     pass
