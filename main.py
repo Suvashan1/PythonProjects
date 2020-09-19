ec1c64b29435c70bd2a451c6d1f422459b7b3c71
Suvashan1 <suvapillay@gmail.com>
2020-09-19 12:33:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file('design.kv')
+
+class LoginScreen(Screen):          #Third highest priority
+    pass    
+
+class RootWidget(ScreenManager):   #Second highest priority
+    pass
+
+class MainApp(App):             #Highest priority in heirachy in Kivy Apps
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    MainApp().run()
