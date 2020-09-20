686d84fbe3696061980329e7b19bc228298e6217
Suvashan1 <suvapillay@gmail.com>
2020-09-20 09:57:21 +0200

    UI Design

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-#:kivy 2.0.0
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file('C:\\Users\\Suvashan\\Desktop\\stm32\PythonProjects\\Mobile Mood App\\design.kv')
+Builder.load_file('C:\\Users\\Suvashan\\Documents\\Python\\design.kv')
 
 class LoginScreen(Screen):          #Third highest priority
-    def signup(self):
-        print("Hey")  
+    def sign_up(self):
+        self.manager.current = "signup_screen"   #name of SignUpScreen is used when the sign-up button is pressed
+                                                 #the sign_up function is called from the button press
 
-class RootWidget(ScreenManager):   #Second highest priority
+
+class SignUpScreen(Screen):             #Instance of Screen, used to initialize the sign-up screen
+    def add_user(self, name, passw):
+        print(name, passw)
+        
+
+class RootWidget(ScreenManager):   #Parent creation of the app object        
     pass
 
-class MainApp(App):             #Highest priority in heirachy in Kivy Apps
+class MainApp(App):             #Highest priority in heirachy in Kivy Apps which returns the app 
     def build(self):
         return RootWidget()
 
